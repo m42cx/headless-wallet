d30e09a9b98b498ce6c622e28d0ceba58d5a14b2
Eugene Ilagin <eugene.ilagin@gmail.com>
2018-03-30 13:29:13 +0300

    Create airbnb linter for headless wallet #957

--- conf.js
+++ conf.js
@@ -1,8 +1,9 @@
-/*jslint node: true */
-"use strict";
+/* jslint node: true */
 
-//exports.port = 6611;
-//exports.myUrl = 'wss://mydomain.com/bb';
+'use strict';
+
+// exports.port = 6611;
+// exports.myUrl = 'wss://mydomain.com/bb';
 exports.bServeAsHub = false;
 exports.bLight = false;
 
@@ -18,11 +19,11 @@ exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 exports.KEYS_FILENAME = 'keys.json';
 
 // where logs are written to (absolute path).  Default is log.txt in app data directory
-//exports.LOG_FILENAME = '/dev/null';
+// exports.LOG_FILENAME = '/dev/null';
 
 // consolidate unspent outputs when there are too many of them.  Value of 0 means do not try to consolidate
 exports.MAX_UNSPENT_OUTPUTS = 0;
-exports.CONSOLIDATION_INTERVAL = 3600*1000;
+exports.CONSOLIDATION_INTERVAL = 3600 * 1000;
 
 // this is for runnining RPC service only, see play/rpc_service.js
 exports.rpcInterface = '127.0.0.1';
@@ -30,9 +31,9 @@ exports.rpcPort = '6332';
 
 console.log('finished headless conf');
 
-//Whether to redirect the output stream to a log file
-exports.PIPE_OUTPUT_TO_FILE=false;
-//Whether to use a password from the configuration file instead of asking it from the user
-exports.INTERACT_WITH_USER=false;
-//Wallet password. Relevant only in case INTERACT_WITH_USER is false.
-exports.WALLET_PASSWORD='BSh5COP5ZesCgmZPyPmB'; //Mock password.
+// Whether to redirect the output stream to a log file
+exports.PIPE_OUTPUT_TO_FILE = false;
+// Whether to use a password from the configuration file instead of asking it from the user
+exports.INTERACT_WITH_USER = false;
+// Wallet password. Relevant only in case INTERACT_WITH_USER is false.
+exports.WALLET_PASSWORD = 'BSh5COP5ZesCgmZPyPmB'; // Mock password.
