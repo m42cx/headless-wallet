43a2718905f4e68d4d87816e24fb432bf53210e8
Fishera123 <lasha.tsintsabadze@gmail.com>
2018-05-08 13:29:39 +0300

    added livenet hub in conf.js

--- conf.js
+++ conf.js
@@ -11,7 +11,7 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'test-hub.dagcoin.org/spoon/';
+exports.hub = 'hub.dagcoin.link';
 exports.deviceName = 'Headless Wallet';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
@@ -25,7 +25,7 @@ exports.KEYS_FILENAME = 'keys.json';
 exports.MAX_UNSPENT_OUTPUTS = 0;
 exports.CONSOLIDATION_INTERVAL = 3600 * 1000;
 
-// this is for runnining RPC service only, see play/rpc_service.js
+// this is for runnining RPC service on dagly, see play/rpc_service.js
 exports.rpcInterface = '127.0.0.1';
 exports.rpcPort = '6332';
 
@@ -34,6 +34,6 @@ console.log('finished headless conf');
 // Whether to redirect the output stream to a log file
 exports.PIPE_OUTPUT_TO_FILE = false;
 // Whether to use a password from the configuration file instead of asking it from the user
-exports.INTERACT_WITH_USER = false;
+exports.INTERACT_WITH_USER = true;
 // Wallet password. Relevant only in case INTERACT_WITH_USER is false.
-exports.WALLET_PASSWORD = 'BSh5COP5ZesCgmZPyPmB'; // Mock password.
+// exports.WALLET_PASSWORD = 'BSh5COP5ZesCgmZPyPmB'; // Mock password.
