22f8cbb1e19719aa0974369c8504b2e3314acc3d
Fishera123 <lasha.tsintsabadze@ignite.ee>
2018-05-21 17:41:25 +0300

    cleanup

--- play/rpc_service.js
+++ play/rpc_service.js
@@ -3,7 +3,7 @@
 /*
 	Accept commands via JSON-RPC API.
 	The daemon listens on port 6332 by default.
-	See https://github.com/byteball/headless-byteball/wiki/Running-RPC-service for detailed description of the API
+	See https://github.com/byteball/headless-wallet/wiki/RPC-service for detailed description of the API
 */
 
 "use strict";
